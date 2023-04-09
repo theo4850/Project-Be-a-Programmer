be8ada5a0a592283c6c47e66711c5dc80cc7974b
Theo Diamand <theodiamand100@gmail.com>
2023-04-09 19:14:25 +0300

    Initial commit

---
+++ main app.py
@@ -0,0 +1,3 @@
+print("Hello, this is the Be a programmer app, \n")
+print("the first algorithm that gives the user personalised advices and recommendations \n")
+print("about which path you should follow, based on the user's responses"!"")
