dd1177586a2b124da6879129fd2d5e31655c5523
Theo Diamand <theodiamand100@gmail.com>
2023-04-09 19:17:30 +0300

    User input is added

--- main app.py
+++ main app.py
@@ -1,3 +1,5 @@
 print("Hello, this is the Be a programmer app, \n")
 print("the first algorithm that gives the user personalised advices and recommendations \n")
-print("about which path you should follow, based on the user's responses"!"")
+print("about which path you should follow, based on the user's responses!")
+
+user_input = input("What type of developer you want to be? Please answer here: ")
